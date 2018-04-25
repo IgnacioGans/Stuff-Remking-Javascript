98fbf0945a4485a80b4ce6e72789aa3d50917f55
Ignacio Gutiérrez <gans1988igna@gmail.com>
2018-04-25 13:26:45 -0400

    Update Script Stuff

--- Script_Remking.js
+++ Script_Remking.js
@@ -8,3 +8,85 @@ if (rk_complete && rk_class) {
 }
 
 }, 10000);
+
+
+
+/*------------------------*/
+
+var rk_letter1 = "abcde"
+rk_letter1.repeat()
+//--> ""
+rk_letter1.repeat(2)
+//--> "abcdeabcde"
+rk_letter1.repeat(2)
+//"abcdeabcde"
+
+/*-----------------------*/
+var rk_letter1 = "abcde"
+rk_letter1.substring(0)
+//"abcde"
+rk_letter1.substring(1)
+//"bcde"
+rk_letter1.substring(2)
+//"cde"
+rk_letter1.substring(3)
+//"de"
+rk_letter1.substring(4)
+//"e"
+
+/*-----------------------*/
+
+var rk_letter1 = "abcde"
+undefined
+for (var i = 0; i < rk_letter1; i++){
+	rk_letter1.charAt(i);
+}
+//undefined
+rk_letter1[0]
+//"a"
+rk_letter1[1]
+//"b"
+rk_letter1[2]
+//"c"
+rk_letter1[3]
+//"d"
+rk_letter1[4]
+//"e"
+rk_letter1[5]
+//undefined
+
+/*---------------------*/
+var veggies = "carrot";
+		var fruitvegbasket = [];
+
+		//console.log(fruitvegbasket.push(veggies));
+		//console.log(fruitvegbasket);
+
+		for (var i = 0; i < veggies.length; i++){			
+			fruitvegbasket.push(i);
+			//console.log(fruitvegbasket);
+		}
+
+
+		var rk_letter1 = "abcde";
+		var rk_eachLetter = [];
+		var rk_eachLetterBySplit = [];
+		var rk_Split = rk_letter1.split('');
+		console.info("Method Split: "+ rk_Split);
+		console.log("Method Array Location: " + rk_letter1[0]);
+		console.log("Method Push With Split: " + rk_eachLetterBySplit.push(rk_Split));
+		console.log("Method Push With Split [Value]: " + rk_eachLetterBySplit);
+
+		for (var i = 0; i < rk_letter1.length; i++){
+			
+			//console.log(rk_letter1.charAt(i));
+			var letterByletter = rk_letter1.charAt(i);
+			var rk_pushToArray = rk_eachLetter.push(letterByletter);
+			//console.log(rk_pushToArray);
+			//console.log(rk_eachLetter);
+		}
+
+		console.log("Method For + charAt + Push " +rk_eachLetter);
+
+
+
